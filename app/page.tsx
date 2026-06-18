b8873bcbf0a170d5b63bdfd5c133077bc730fb94
Praveenganth <praveenganth@plenome.com>
2026-06-18 23:57:00 +0530

    page content changed

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,11 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Portfolio is getting ready
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            A place to show case my work and who i am 
+            {/* 
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -31,7 +32,7 @@ export default function Home() {
             >
               Learning
             </a>{" "}
-            center.
+            center. */}
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
